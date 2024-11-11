d8e1a81c2c6b2cbf642b90a080e2f1f3b08c5980
GabrielIndrawan <Abelepic3@gmail.com>
2024-11-11 09:48:58 +0700

    revisi

---
+++ flutter_application_1 - Copy/lib/pages/telpon.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/default_app_bar.dart';
+
+class Telephone extends StatelessWidget {
+  const Telephone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const DefaultAppBar(),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.only(top:10),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Our Number :", style: TextStyle(color: Colors.indigo, fontSize: 20, fontWeight: FontWeight.bold),),
+              SizedBox(height: 25,),
+              Text("889-xxxx-xxx", style: TextStyle(color: Colors.indigo, fontSize: 20, fontWeight: FontWeight.bold),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
