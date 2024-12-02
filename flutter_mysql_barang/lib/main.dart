1f15c6d42a38fb41cd5f5b930a0fc7bf788919fb
GabrielIndrawan <111202213979@mhs.dinus.ac.id>
2024-12-02 11:56:23 +0700

    menambah tampilan

--- flutter_mysql_barang/lib/main.dart
+++ flutter_mysql_barang/lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mysql_barang/pages/home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
-    );
-  }
+void main() => runApp(App());
+class App extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ return MaterialApp(
+ debugShowCheckedModeBanner: false,
+ // themeMode: ThemeMode.dark,
+ theme: ThemeData(
+ primaryColor: Colors.black,
+ scaffoldBackgroundColor: Colors.blueGrey.shade900, 
+ appBarTheme:
+ AppBarTheme(backgroundColor: Colors.transparent, elevation: 0)),
+ title: 'Flutter + PHP CRUD',
+ initialRoute: '/',
+ routes: {
+ '/': (context) => Home(),
+ },
+ );
+ }
 }
